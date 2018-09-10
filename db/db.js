f381e0fb87ed707cbfcf43aba7c224282eb20816
llc <pratik@ideas2tech.com>
2018-09-10 13:52:59 +0530

    commited

--- db/db.js
+++ db/db.js
@@ -3,7 +3,7 @@ const MongoClient = require('mongodb').MongoClient
 const PROD_URI = "mongodb://localhost:27017/mdcl"
 
 function connect(url) {
-    return MongoClient.connect(url).then(client => client.db())
+    return MongoClient.connect(url, { useNewUrlParser: true }).then(client => client.db())
 }
 
 module.exports = async function () {
